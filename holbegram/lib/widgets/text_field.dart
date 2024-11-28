3428bf0ed0ea0bdf4c36b268dab5f84c7cc751c0
MariamCHARRADA <charradamariam@yahoo.com>
2024-11-28 22:55:01 +0100

    text widget

---
+++ holbegram/lib/widgets/text_field.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class TextFieldInput extends StatelessWidget {
+  final TextEditingController controller;
+  final bool isPassword;
+  final String hintText;
+  final Widget? suffixIcon;
+  final TextInputType keyboardType;
+
+  const TextFieldInput({
+    Key? key,
+    required this.controller,
+    required this.isPassword,
+    required this.hintText,
+    this.suffixIcon,
+    required this.keyboardType,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: isPassword,
+      keyboardType: keyboardType,
+      controller: controller,
+      cursorColor: const Color.fromARGB(218, 226, 37, 24),
+      decoration: InputDecoration(
+          hintText: hintText,
+          filled: true,
+          contentPadding: const EdgeInsets.all(8),
+          suffixIcon: suffixIcon,
+          border: const OutlineInputBorder(
+            borderSide: BorderSide.none,
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide.none,
+          ),
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide.none,
+          )),
+      textInputAction: TextInputAction.next,
+    );
+  }
+}
